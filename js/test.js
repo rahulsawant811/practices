650df5dca9a75ad740324c9a61457c89fd44b860
Rahul Sawant <Rahul.Sawant@salebuild.com>
2017-08-21 16:44:39 -0400

    Add element remove element using splice

--- js/test.js
+++ js/test.js
@@ -30,5 +30,28 @@ var materials = [
   'Beryllium'
 ];
 
-
 console.log(materials.map(material => material.length)); // [8, 6, 7, 9]
+
+// Comparison operator
+
+let a = {};
+let b = {};
+
+// console.log(typeof(a)); Object
+// console.log(typeof(b)); Object
+
+if(a === b){
+    console.log('Pass');
+}else{
+    console.log('Fail');
+}
+
+// Add element in between
+var arr1 = [1,2,3];
+arr1.splice(2, 0, 5);
+console.log(arr1); // [1,2,5,3]
+
+// Remove element from index
+var arr2 = [1,2,3];
+arr2.splice(1, 1);
+console.log(arr2); // [1,3]
